6c9bdd4cbce30639ac01e70243d93d93b900d246
Junseok <oh971021@gmail.com>
2021-10-19 12:24:01 +0900

    1019 Self-Project : Fortune ( Make horoscope )

---
+++ js/scope.js
@@ -0,0 +1,11 @@
+function callScope() {
+    location.href='fortuneSite/horoscope.html';
+
+    whatScope();
+}
+
+function whatScope() {
+    
+    let mouse = "hello";
+    document.getElementById('scopeAnswer').innerText = mouse;
+}
