0e5de49f15be51d3d08087049897bc49df519b87
Junseok <oh971021@gmail.com>
2021-10-21 00:22:23 +0900

    1021 Self-Project : Fortune ( Add Notice Page & Css )

--- js/notice.js
+++ js/notice.js
@@ -1,3 +1,11 @@
 function search() {
-    alert("미구현")
+    let search = document.getElementById('search');
+    search.value = null;
+    search.focus();
+    alert("미구현");
+    return false;
+}
+
+function add() {
+    alert("미구현");
 }
